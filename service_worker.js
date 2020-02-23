a2f90c8fde764700b50aab94c0b252d960a2413e
Yauhen Pyl <gromkaktus@gmail.com>
2020-02-23 21:43:36 +0100

    Add service worker

---
+++ service_worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('sudoku').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/manifest.json',
+        '/elm.js',
+        '/assets/images/dwyl.png',
+        '/assets/images/signal_wifi_off.svg',
+        '/assets/css/tachyons.css',
+        '/assets/css/app.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
